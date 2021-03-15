509b95e1e740e91da02ff7dcbc12a02963f17e75
Banushan <banushanv@gmail.com>
2021-03-15 21:05:25 +0530

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+  This is a vite Project
+<button @click="counter++" >{{counter}}</button>
+  </div>
+</template>
+
+<script>
+import {ref} from 'vue';    
+export default {
+  setup () {
+ const counter=ref(344);  
+
+    return {
+      counter
+    };
+  }
+}
+</script>
+
+<style>
+
+</style>
